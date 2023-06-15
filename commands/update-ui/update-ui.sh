b2fffef389bcfa6d66c4c6f5356c0a6bc01d36c7
Mira Ressel <mira@parity.io>
2023-06-16 00:23:13 +0200

    feat: new update-ui command
    This command updates substrate's UI tests, which is a routine step we
    have to perform during every rust toolchain upgrade.

---
+++ commands/update-ui/update-ui.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+set -eu -o pipefail
+shopt -s inherit_errexit
+
+. "$(dirname "${BASH_SOURCE[0]}")/../utils.sh"
+. "$(dirname "${BASH_SOURCE[0]}")/../cmd_runner.sh"
+
+main() {
+  # setup.
+  cmd_runner_setup
+
+  rust_version="$1"
+  if [[ -z "${rust_version}" ]]; then
+    die "missing rust version argument"
+  fi
+
+  # This script uses rustup to install the required rust version.
+  # Doing that in CI feels rather ugly, but sadly there's currently no mechanism
+  # in command-bot to use a different CI image for individual jobs, so this is
+  # the best we can do.
+  .maintain/update-rust-stable.sh "${rust_version}"
+
+  # commit.
+  git add .
+  git commit -m "${COMMIT_MESSAGE}"
+
+  # Push the results to the target branch
+  git remote add \
+    github \
+    "https://token:${GITHUB_TOKEN}@github.com/${GH_CONTRIBUTOR}/${GH_CONTRIBUTOR_REPO}.git"
+  git push github "HEAD:${GH_CONTRIBUTOR_BRANCH}"
+}
+
+main "$@"
