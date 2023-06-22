61439a79bb7cc772df9b2f4ed43ab1df0cc5d925
Oleg Plakida <oleg@parity.io>
2023-06-22 13:21:48 +0100

    Switch to weights-vm runners
    - Use `weights-vm` tag as default
    - Use `bench-bot` tag as legacy for bench-bm command

--- commands/bench-bm/bench-vm.sh
+++ commands/bench-bm/bench-vm.sh
@@ -3,5 +3,5 @@
 set -eu -o pipefail
 shopt -s inherit_errexit
 
-# this is a fallback for `bench` command running on BM3 machine
+# this is a fallback for `bench` command running on VM machine
 . "$(dirname "${BASH_SOURCE[0]}")/../bench/bench.sh" "$@"
