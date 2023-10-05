d2c7017bf7ef5ccc5d0927cd2da86ca14dd0b33c
Mira Ressel <mira@parity.io>
2023-10-05 15:33:55 +0200

    chore: Drop references to old bm* runners
    We're finally decommissioning those hosts.

--- commands/bench-bm/bench-bm.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -eu -o pipefail
-shopt -s inherit_errexit
-
-# this is a fallback for `bench` command running on VM machine
-. "$(dirname "${BASH_SOURCE[0]}")/../bench/bench.sh" "$@"
