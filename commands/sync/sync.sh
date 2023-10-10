93cac25d6acd89801d63e912f5ebdb21faaf1aa2
Mak <mordamax@gmail.com>
2023-10-10 14:56:58 +0100

    sync chain

--- commands/sync/sync.sh
+++ commands/sync/sync.sh
@@ -9,23 +9,19 @@ main() {
   # setup.
   cmd_runner_setup
 
-  get_arg optional --chain "$@"
+  get_arg required --chain "$@"
   local chain="${out:-""}"
+
+  get_arg required --type "$@"
+  local type="${out:-""}"
 
   set -x
   export RUST_LOG="${RUST_LOG:-remote-ext=debug,runtime=trace}"
 
   cargo build --release
 
-  "./target/release/polkadot sync --chain=$chain"
-
-  git add .
-  git commit -m "$COMMIT_MESSAGE"
-
-  git remote add \
-    github \
-    "https://token:${GITHUB_TOKEN}@github.com/${GH_CONTRIBUTOR}/${GH_CONTRIBUTOR_REPO}.git"
-  git push github "HEAD:${GH_CONTRIBUTOR_BRANCH}"
+  cp "./target/release/polkadot" polkadot
+  ./polkadot --sync="$type" --chain="$chain"
 }
 
 main "$@"
