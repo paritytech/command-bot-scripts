7d29ef294687cfb2f4266e182dd9e63b97b14096
Mak <mordamax@gmail.com>
2024-01-24 20:47:36 +0000

    add bench all pallet to bench-all

--- commands/bench-all/bench-all.sh
+++ commands/bench-all/bench-all.sh
@@ -2,5 +2,17 @@
 
 set -eu -o pipefail
 shopt -s inherit_errexit
+shopt -s globstar
 
-. "$(dirname "${BASH_SOURCE[0]}")/../bench/bench.sh" --subcommand=all "$@"
+. "$(dirname "${BASH_SOURCE[0]}")/../utils.sh"
+. "$(dirname "${BASH_SOURCE[0]}")/../cmd_runner.sh"
+
+get_arg optional --pallet "$@"
+PALLET="${out:-""}"
+
+if [[ ! -z "$PALLET" ]]; then
+  . "$(dirname "${BASH_SOURCE[0]}")/lib/bench-all-pallet.sh" "$@"
+else
+  . "$(dirname "${BASH_SOURCE[0]}")/../bench/bench.sh" --subcommand=all "$@"
+fi
+
