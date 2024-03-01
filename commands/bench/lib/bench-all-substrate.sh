aa987e18f6180ef5e2b25d4ac1298ce25d486c78
georgepisaltu <george.pisaltu@parity.io>
2024-03-01 11:26:39 +0200

    Update bench-all-substrate
    Signed-off-by: georgepisaltu <george.pisaltu@parity.io>

--- commands/bench/lib/bench-all-substrate.sh
+++ commands/bench/lib/bench-all-substrate.sh
@@ -94,6 +94,16 @@ fi
 for PALLET in "${PALLETS[@]}"; do
   FOLDER="$(echo "${PALLET#*_}" | tr '_' '-')";
   WEIGHT_FILE="$output_path/frame/${FOLDER}/src/weights.rs"
+
+  # Special handling of custom weight paths.
+  if [ ["$PALLET" == "frame_system_extensions" || "$PALLET" == "frame-system-extensions"] ]; then
+    WEIGHT_FILE="$output_path/frame/system/src/extensions/weights.rs"
+  elif [ ["$PALLET" == "pallet_asset_conversion_tx_payment" || "$PALLET" == "pallet-asset-conversion-tx-payment"] ]; then
+    WEIGHT_FILE="$output_path/frame/transaction-payment/asset-conversion-tx-payment/src/weights.rs"
+  elif [ ["$PALLET" == "pallet_asset_tx_payment" || "$PALLET" == "pallet-asset-tx-payment"] ]; then
+    WEIGHT_FILE="$output_path/frame/transaction-payment/asset-tx-payment/src/weights.rs"
+  fi
+
   echo "[+] Benchmarking $PALLET with weight file $WEIGHT_FILE";
 
   OUTPUT=$(
